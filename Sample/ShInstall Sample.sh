f0c04ddaea8660d66b6d260491620cde83df085e
enderger <endergeryt@gmail.com>
2018-04-21 07:16:33 -0500

    Tweaked a few small things

--- Sample/ShInstall Sample.sh
+++ Sample/ShInstall Sample.sh
@@ -1,16 +1,15 @@
-#/bin/sh
+#!/bin/sh
  CAN_I_RUN_SUDO=$(sudo -n uptime 2>&1|grep "load"|wc -l)
  if [ ${CAN_I_RUN_SUDO} -gt 0 ]
  then
 	 echo "Sudo test succeded!"
  else
-	 echo "You do not have sudo installed, or you can't use it."
+	 echo "You do not have sudo installed, or you aren't using it."
      exit
  fi
  sudo -v
  echo "Welcome to the ShInstall Demo!"
- #start copying here!
- echo "ShInstall version 18.04 b1 from GalactiDev"
+ echo "ShInstall version 18.04 (beta 0.0.1) from GalactiDev"
  echo "Prepairing to install the program test1 from the files in the online website https://bit.ly/2HGVWmf." #Change the last part of this line to your download link for the file.
  read -p "Do you wish to continue?(Y/n)" yn
  case $yn in
@@ -21,7 +20,7 @@ wget https://github.com/enderger/ShInstall-system/raw/master/Sample/ShInstall%20
 sudo tar -xvf "ShInstall Sample Download.tar.gz" 
 sudo chown -R $SUDO_USER "ShInstall Sample Download"  #add this line with the name of the extracted archive!
 cd "ShInstall Sample Download" 
-mkdir /home/$SUDO_USER/Desktop/"Desktop Folder!"/ #For any mkdir, make sure to grant the sudoer ownership of the folder!
+mkdir /home/$SUDO_USER/Desktop/"Desktop Folder!"/ #for any mkdir, make sure to grant the sudoer ownership of the folder!
 sudo chown $SUDO_USER /home/$SUDO_USER/Desktop/"Desktop Folder!"
 mv "Desktop folder!"/* /home/$SUDO_USER/Desktop/"Desktop Folder!"
 rm -rf "Desktop folder!"
---
+++ Sample/ShInstallBase.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+CAN_I_RUN_SUDO=$(sudo -n uptime 2>&1|grep "load"|wc -l)
+ if [ ${CAN_I_RUN_SUDO} -gt 0 ]
+ then
+	 echo "Sudo test succeded!"
+ else
+	 echo "You do not have sudo installed, or you aren't using it."
+     exit
+ fi
+ sudo -v
+ echo "Welcome to the ShInstall Demo!"
+ echo "ShInstall version 18.04 (beta 0.0.1) from GalactiDev"
+ echo "Prepairing to install the program PROGRAM NAME HERE! from the files in the online website INSERT LINK HERE." #Change the part labeled PROGRAM NAME HERE! with the name of your program and the last part of this line to your download link for the file.
+ read -p "Do you wish to continue?(Y/n)" yn
+ case $yn in
+	[Nn]* ) exit;;
+	esac
+#insert your install code below
