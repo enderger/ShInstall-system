5d4ac9b884831d6075477f12a7980ee42e7c4637
enderger <36283171+enderger@users.noreply.github.com>
2018-04-25 15:02:03 -0500

    Fixed the out-of-context demo word

--- ShInstallBase.sh
+++ ShInstallBase.sh
@@ -7,7 +7,7 @@ CAN_I_RUN_SUDO=$(sudo -n uptime 2>&1|grep "load"|wc -l)
 	 echo "You do not have sudo installed, or you aren't using it."
      exit
  fi
- echo "Welcome to the ShInstall Demo!"
+ echo "Welcome to the ShInstall Install System!"
  echo "ShInstall version 18.04 (beta 0.0.1) from GalactiDev"
  echo "Prepairing to install the program PROGRAM NAME HERE! from the files in the online website INSERT LINK HERE." #Change the part labeled PROGRAM NAME HERE! with the name of your program and the last part of this line to your download link for the file.
  read -p "Do you wish to continue?(Y/n)" yn
